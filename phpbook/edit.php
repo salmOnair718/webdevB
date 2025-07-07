2b3222a4b3137dfd5b9f21af3cb0609def575540
Inazawa Erika <salmonair718@gmail.com>
2025-07-07 13:54:41 +0900

    これから最終課題

--- phpbook/edit.php
+++ phpbook/edit.php
@@ -6,7 +6,7 @@ $_SESSION['token'] = $token;
 ?>
 <?php
 require_once __DIR__ . '/inc/functions.php';
-require_once __DIR__ . '/login_check.php';
+// require_once __DIR__ . '/login_check.php';
 
 if (empty($_GET['id'])) {
   echo "idを指定してください。";
