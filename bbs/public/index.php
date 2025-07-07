d3fd3a2d4d0e4a4acc6b929ac284ce067602d6a9
Inazawa Erika <salmonair718@gmail.com>
2025-07-07 16:51:46 +0900

    070720thHPB

---
+++ bbs/public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once __DIR__ . '/../functions.php';
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../app/models/Post.php';
+require_once __DIR__ . '/../app/controllers/PostController.php';
